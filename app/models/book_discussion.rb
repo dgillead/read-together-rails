bb360f087a77394f8affe2825539910b02d2b6c0
Darnell Gillead <dlgillead@gmail.com>
2017-08-02 09:22:44 -0500

    added activerecord validations

--- app/models/book_discussion.rb
+++ app/models/book_discussion.rb
@@ -1,4 +1,10 @@
 class BookDiscussion < ApplicationRecord
+  validates :user_id, presence: true
+  validates :book_title, presence: true
+  validates :book_image_url, presence: true
+  validates :book_author, presence: true
+  validates :status, presence: true
+
   has_many :sections, dependent: :destroy
   belongs_to :user
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,6 @@
 class Section < ApplicationRecord
+  validates :book_discussion_id, presence: true
+  validates :title, presence: true
+
   belongs_to :book_discussion
 end
