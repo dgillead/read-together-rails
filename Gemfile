3afcba6e62ffec173a70b9a5a6796b1070a670b8
Darnell Gillead <dlgillead@gmail.com>
2017-08-02 10:00:20 -0500

    created test for BookControllers#index

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'pry'
+  gem 'database_cleaner'
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- spec/controllers/book_discussions_controller_spec.rb
+++ spec/controllers/book_discussions_controller_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
 
 RSpec.describe BookDiscussionsController, type: :controller do
   render_views
@@ -8,17 +11,19 @@ RSpec.describe BookDiscussionsController, type: :controller do
   }
 
   let(:valid_book_attributes) {
-    { book_title: 'The Book Title', book_image_url: 'www.bookimage.com', book_author: 'me', status: 'public', discussion_participants: ['dg@gmail.com'] }
+    { book_title: 'The Book Title', book_image_url: 'https://images.gr-assets.com/books/1344371661m/6424171.jpg', book_author: 'me', status: 'public', discussion_participants: ['dg@gmail.com'] }
   }
 
   let(:invalid_book_attributes) {
     { book_title: nil }
   }
 
+
   describe 'GET #index' do
     it 'assigns the discussions the user is included in in @book_discussions' do
       user = User.create!(valid_user_attributes)
-      binding.pry
+      sign_in(user)
+      valid_book_attributes[:user_id] = user.id
       book_discussion = BookDiscussion.create!(valid_book_attributes)
 
       get :index
@@ -26,4 +31,6 @@ RSpec.describe BookDiscussionsController, type: :controller do
       expect(assigns(:book_discussions)).to eq([book_discussion])
     end
   end
+
+  DatabaseCleaner.clean
 end
