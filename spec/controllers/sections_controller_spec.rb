b214d47c270259e21a16f4dfe6132b900c76dc1f
Darnell Gillead <dlgillead@gmail.com>
2017-08-02 15:55:59 -0500

    added test for Section#create action

--- spec/controllers/sections_controller_spec.rb
+++ spec/controllers/sections_controller_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe SectionsController, type: :controller, vcr: true do
   let(:user) { User.create!(first_name: 'D', last_name: 'G', email: 'dg@gmail.com', password: 'asdfasdf') }
   let(:book_discussion) { BookDiscussion.create!(book_title: 'The Book Title', book_image_url: 'https://images.gr-assets.com/books/1344371661m/6424171.jpg', book_author: 'me', status: 'public', discussion_participants: ['dg@gmail.com'], user_id: user.id) }
 
+  let(:valid_section_attributes) { { title: 'Title' } }
+
 
   describe 'GET #new' do
     it 'assigns a section as @section' do
@@ -20,6 +22,13 @@ RSpec.describe SectionsController, type: :controller, vcr: true do
     end
   end
 
+  describe 'POST #create' do
+    it 'creates the new section record and assigns it to @section' do
+      sign_in(user)
+
+      expect{ post :create, params: { book_discussion_id: book_discussion.to_param, section: valid_section_attributes } }.to change{ book_discussion.sections.count }.by(1)
+    end
+  end
 
   DatabaseCleaner.clean
 end
