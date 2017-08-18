2c610461d2c735040090867577a1fb50a0d4d3ac
Darnell Gillead <dlgillead@gmail.com>
2017-08-17 19:57:29 -0500

    fixed issue with password reset token

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :book_discussions
+
+  def self.find_first_by_auth_conditions warden_conditions
+    conditions = warden_conditions.dup
+    if (email = conditions.delete(:email)).present?
+      where(email: email.downcase).first
+    elsif conditions.has_key?(:reset_password_token)
+      where(reset_password_token: conditions[:reset_password_token]).first
+    end
+  end
 end
